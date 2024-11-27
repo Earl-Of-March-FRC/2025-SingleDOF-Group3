dcf3dc6a3cd24794302408a2bef798c727d58d04
chairzihan <146217784+chairzihan@users.noreply.github.com>
2024-11-26 20:45:49 -0500

    Snowberry

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import frc.robot.Constants.ShooterConstants;
+
+
+public class Shooter extends SubsystemBase {
+
+  private final WPI_VictorSPX shooterMotor = new WPI_VictorSPX(ShooterConstants.motorPort);
+  /** Creates a new Shooter. */
+  public Shooter() {
+     shooterMotor.setNeutralMode(NeutralMode.Brake);
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+
+  public void setMotorSpeed(double speed){
+    //Maybe move to robot container
+    MathUtil.clamp(speed, -ShooterConstants.maxMotorSpeed, ShooterConstants.maxMotorSpeed);
+    shooterMotor.set(speed);
+
+  }
+}
