010c799c04c9d63eaa8214fe4875a6054244f84c
Emily0727 <148280630+Emily0727@users.noreply.github.com>
2024-11-28 19:57:46 -0500

    encoder_test1

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,18 +7,21 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
-
+//import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.Constants.ShooterConstants;
 
 
 public class Shooter extends SubsystemBase {
 
-  private final WPI_VictorSPX shooterMotor = new WPI_VictorSPX(ShooterConstants.motorPort);
+  private final TalonSRX shooterMotor = new TalonSRX(ShooterConstants.motorPort)
+
+ // private final WPI_VictorSPX shooterMotor = new WPI_VictorSPX(ShooterConstants.motorPort);
   /** Creates a new Shooter. */
   public Shooter() {
      shooterMotor.setNeutralMode(NeutralMode.Brake);
@@ -29,10 +32,13 @@ public class Shooter extends SubsystemBase {
     
   }
 
+  /**
+   * @param speed
+   */
   public void setMotorSpeed(double speed){
     //Maybe move to robot container
     MathUtil.clamp(speed, -ShooterConstants.maxMotorSpeed, ShooterConstants.maxMotorSpeed);
-    shooterMotor.set(speed);
+    shooterMotor.set(ControlMode.Velocity,speed);
 
   }
 }
